0f962797e366252ed06f012abb9e6e1a9e4e65b5
Kenshi Muto <kmuto@debian.org>
2013-10-07 23:15:18 +0900

    エピローグを本章に移し、採番はしない。目次に入れるためにreview-ext.rbをハック(場当たり対処なのでReVIEW側でちゃんと直すべき)

--- review-ext.rb
+++ review-ext.rb
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 module ReVIEW
   class HTMLBuilder
+    def nonum_begin(level, label, caption)
+      puts '' if level > 1
+      unless caption.empty?
+        if label.nil?
+          puts %Q[<h#{level}><a id="hn#{level}"></a>#{compile_inline(caption)}</h#{level}>]
+        else
+          puts %Q[<h#{level} id="#{label}"><a id="hn#{level}"></a>#{compile_inline(caption)}</h#{level}>]
+        end
+      end
+    end
+
     def inline_fn(id)
       %Q(<a href="#fn-#{id}" class="footnotemark">*#{@chapter.footnote(id).number}</a>)
     end
