ba258eba05d1d10394889eed28a517da4b27b771
takahashim <maki@rubycolor.org>
2014-07-03 00:19:16 +0900

    LATEX版のfootnoteの修正 cf. #16

--- review-ext.rb
+++ review-ext.rb
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 module ReVIEW
+  class LATEXBuilder
+    def footnote(id, content)
+      if @book.config["footnotetext"]
+        puts macro("footnotetext[#{id}]",
+                   compile_inline(content.strip))
+      end
+    end
+
+    def inline_fn(id)
+      if @book.config["footnotetext"]
+        macro("footnotemark[#{@chapter.footnote(id).number}]", "")
+      else
+        macro("footnote[#{id}]", compile_inline(@chapter.footnote(id).content.strip))
+      end
+    end
+
+  end
+
   class HTMLBuilder
     def nonum_begin(level, label, caption)
       puts '' if level > 1
